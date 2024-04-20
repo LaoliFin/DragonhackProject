1d4bd990012cf3b1d45c39e1935afce5e9d159c9
gitnubnub <urska.krumpak@gmail.com>
2024-04-20 15:37:39 +0200

    event card component

--- Telebytes/src/app/app.module.ts
+++ Telebytes/src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { EventPageComponent } from './components/event-page/event-page.component';
 
 @NgModule({
   declarations: [
     
+  
+    EventPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: "eventpage", component: EventPageComponent },
+    ]),
   ],
   providers: [],
   bootstrap: []
