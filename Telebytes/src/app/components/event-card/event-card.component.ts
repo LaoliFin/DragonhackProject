50f30e5bfe9515a17e75073c3184200bc03eded1
laralogar <logar.lara@gmail.com>
2024-04-21 01:27:41 +0200

    add comment

--- Telebytes/src/app/components/event-card/event-card.component.ts
+++ Telebytes/src/app/components/event-card/event-card.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'event-card',
   templateUrl: './event-card.component.html',
   styleUrls: ['./event-card.component.css']
 })
-export class EventCardComponent {
 
+export class EventCardComponent {
+  isFormVisible: boolean = false;
+  textInputValue: string= '';
+
+  @ViewChild('myForm') myForm!: NgForm;
+
+  showForm() {
+    var x: any = document.getElementById("addText");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+  saveForm() {
+    this.isFormVisible = false;
+  }
 }
