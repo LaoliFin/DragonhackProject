edaccb167f4d65dddec823e25235ca15e30ef860
LaoliFin <sara.strakl4@gmail.com>
2024-04-20 15:36:21 +0200

    vse

--- Telebytes/src/app/app.module.ts
+++ Telebytes/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CultureComponent } from './components/culture/culture.component';
 
 @NgModule({
   declarations: [
     
+  
+    CultureComponent
   ],
   imports: [
     BrowserModule
