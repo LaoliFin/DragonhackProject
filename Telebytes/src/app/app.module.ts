6c600dff729600697a081ed5cfe5d6561ca92922
laralogar <logar.lara@gmail.com>
2024-04-20 16:34:06 +0200

    mapa

--- Telebytes/src/app/app.module.ts
+++ Telebytes/src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+//import { AgmCoreModule } from '@agm/core';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 
 @NgModule({
   declarations: [
     
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LeafletModule
+    /*AgmCoreModule.forRoot({
+      apiKey: ''
+    })*/
   ],
   providers: [],
   bootstrap: []
