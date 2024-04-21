cb82779f8177bc63941b0b4f540d75bfbb132efc
LaoliFin <sara.strakl4@gmail.com>
2024-04-21 03:10:26 +0200

    fake login in register

--- Telebytes/src/app/components/login/login.component.ts
+++ Telebytes/src/app/components/login/login.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/compat/auth'; 
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
 import * as firebaseui from 'firebaseui';
 import { Subscription } from 'rxjs';
 import { EmailAuthProvider, GoogleAuthProvider } from 'firebase/auth';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 
 @Component({
@@ -12,5 +13,21 @@ import { EmailAuthProvider, GoogleAuthProvider } from 'firebase/auth';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  
-}
+
+  constructor(
+    private router: Router,
+    private snackBar: MatSnackBar
+    )
+  {
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      this.router.navigate(['/']);
+    }
+    else {
+      this.snackBar.open('Please fill in all required fields', 'Close', {
+        duration: 5000,
+      });}
+  }
+  }
